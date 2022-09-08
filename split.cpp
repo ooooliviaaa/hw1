e466b4a07bab9f0b96dd630b4ea7615b583b1ff5
Huizhen Li <oli46762@usc.edu>
2022-09-08 06:18:00 +0000

    Code changes for problem 1

--- split.cpp
+++ split.cpp
@@ -11,16 +11,121 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <cstddef>
+#include <iostream>
+using namespace std;
 
 /* Add a prototype for a helper function here if you need */
 
-void split(Node*& in, Node*& odds, Node*& evens)
-{
-  /* Add code here */
-// WRITE YOUR CODE HERE
+//void splitHelp(Node*& in, Node*& odds, Node*& evens);
 
+
+// 1. in->next
+// 2. prev node 
+
+//void split(Node*& in, Node*& odds, Node*& evens){
+
+/*  if(in == nullptr){
+    return;
+  }
+  splitHelp(in, odds, evens); 
+}*/
+void split(Node*& in, Node*& odds, Node*& evens){
+  // cout << "split helper function" << endl;
+  
+  // if any of them is nullptr just return 
+  if(in == nullptr){
+    //cout << "odd and even val" <<odds->value <<evens->value<<endl;
+    // if (odds!=nullptr) 
+    odds->next = nullptr;
+    // if (evens!=nullptr) 
+    evens->next = nullptr;
+    return;
+  }
+  
+  //set two boolean so I can easily use them later
+  bool ifEven = in->value % 2 == 0;
+  bool ifOdd = !ifEven;
+
+  //cout << in->value << ifEven<< endl;
+
+  if(ifOdd){
+    if(odds == nullptr){
+        odds = in;
+        split(in->next,odds,evens);
+    }
+    else{ 
+      odds->next = in;
+      split(in->next,odds->next,evens);
+    }
+    //cout << "odds " << odds->value << endl;
+    
+  }
+
+  else if(ifEven){
+    if(evens == nullptr){
+      evens = in;
+      split(in->next,odds,evens);
+    }
+    else{
+      evens->next = in;
+      split(in->next,odds,evens->next);
+    }
+    // cout << "evens " << evens->value << endl;
+    
+  }
+  
 }
 
+
+
+
 /* If you needed a helper function, write it here */
+/*void splitHelp(Node*& in, Node*& odds, Node*& evens){
+  // cout << "split helper function" << endl;
+  
+  // if any of them is nullptr just return 
+  if(in == nullptr){
+    //cout << "odd and even val" <<odds->value <<evens->value<<endl;
+    // if (odds!=nullptr) 
+    odds->next = nullptr;
+    // if (evens!=nullptr) 
+    evens->next = nullptr;
+    return;
+  }
+  
+  //set two boolean so I can easily use them later
+  bool ifEven = in->value % 2 == 0;
+  bool ifOdd = !ifEven;
+
+  //cout << in->value << ifEven<< endl;
+
+  if(ifOdd){
+    if(odds == nullptr){
+        odds = in;
+        split(in->next,odds,evens);
+    }
+    else{ 
+      odds->next = in;
+      split(in->next,odds->next,evens);
+    }
+    //cout << "odds " << odds->value << endl;
+    
+  }
+
+  else if(ifEven){
+    if(evens == nullptr){
+      evens = in;
+      split(in->next,odds,evens);
+    }
+    else{
+      evens->next = in;
+      split(in->next,odds,evens->next);
+    }
+    // cout << "evens " << evens->value << endl;
+    
+  }
+  
+}*/
 
 // WRITE YOUR CODE HERE
--- split.h
+++ split.h
@@ -19,4 +19,6 @@ struct Node
 
 void split(Node*& in, Node*& odds, Node*& evens);
 // WRITE YOUR CODE HERE
+
+
 #endif
